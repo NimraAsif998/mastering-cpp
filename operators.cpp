9859361a01c2ff55a31b86efcdf875a354f01812
Nimra Asif <m68802390@gmail.com>
2026-07-24 13:32:29 +0500

    cpp file

---
+++ operators.cpp
@@ -0,0 +1,81 @@
+// operators
+/*#include<iostream>
+using namespace std;
+int main(){
+
+    /* arithmatic 
+    int num = 2;
+    int num2 =4;
+    cout<<num+num2<<endl;
+    cout<<num-num2<<endl;
+    cout<<num*num2<<endl;
+    cout<<num/num2<<endl;
+    cout<<num%num2<<endl; */
+
+    /*int num1 = 4;
+    cout<<num1<<endl;
+    int num2 = 2;
+    cout<<"after assignmnet op"<<endl;
+    num1-=2;
+    cout<<num1<<endl;
+     // num1 = num1-2
+    num1+=2;
+    cout<<num1<<endl;
+    num1*=2;
+        // num1 = num1*2
+    cout<<num1<<endl;
+    num1/=2;
+    cout<<num1<<endl;
+
+
+    // cout<<num1 > num2 ; // 4 > 2 = tru
+
+    int num =4;
+    num++;
+    cout<<num<<endl;
+
+    int num1 =4;
+   num1--;
+    cout<<num1<<endl;
+
+    
+
+
+
+    return 0;
+}
+
+*/
+
+// question 1
+
+/*#include<iostream>
+using namespace std;
+int main(){
+
+    int studentID = 27;
+    float score = 89.9;
+    char grade = 'A';
+
+    cout<<"student id is :"<<studentID<<endl;
+    cout<<"student score is:"<<score<<endl;
+    cout<<"student grade is :"<<grade<<endl;
+
+
+    return 0;
+}*/
+
+
+// question 2
+
+/*#include<iostream>
+using namespace std;
+int main(){
+
+    int itemPrice = 100;
+    int shippingCost = 50;
+    int sum = itemPrice+shippingCost;
+    cout<<" total cost: "<<sum<<endl;
+    
+    return 0;
+}*/
---
+++ typeCasting.cpp
@@ -0,0 +1,30 @@
+// type casting => implicit type casting 
+/*
+#include<iostream>
+using namespace std;
+int main(){
+
+    int x = 5;
+    int y = 2;
+   int result = x/y; // 2.5
+   cout<<result<<endl;
+//    next line => endl
+
+    return 0;
+}*/
+
+
+
+// explict type casting
+/*#include<iostream>
+using namespace std;
+int main(){
+
+    int a = 3;
+    float b = a;
+    cout<<b;
+
+
+    return 0;
+}
+    */
